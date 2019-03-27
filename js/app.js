4ac79657aa2affa0ca98f536d35bbaa85454dfa6
Devon Hackley <devonhackley@gmail.com>
2019-03-27 16:16:39 -0700

    got question 6 working correctly moving on to 7

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // variables
-const questionArray = ['Do I like to code?', 'Was I in the military?', 'Do I have any siblings?', 'Do I like the rain?', 'Do I have a pet corgi', 'What is my favorite number? Hint it is between 1-10'];
+const questionArray = ['First, what is your name?','Do I like to code?', 'Was I in the military?', 'Do I have a sibling', 'Do I like the rain?', 'Do I have a pet corgi', 'What is my favorite number? Hint it is between 1-10'];
 const possibleAnswersArray = ['Yes', 'y', 'yeah', 'yes', 'No', 'n', 'nah', 'no'];
-const answerArray = ['yes', 'yes', 'yes', 'no', 'yes', '8'];
+const answerArray = ['yes', 'yes', 'yes', 'yes', 'no', 'yes', '8'];
 const form = document.getElementById('guessingGame');
 const questionField = document.getElementById('gameQuestion');
 const resultField = document.getElementById('userResult');
@@ -11,6 +11,7 @@ const answerField = document.getElementById('answerField');
 const nextBtn = document.getElementById('nextQuestion');
 let questionIndex = 0;
 let total = 0;
+let playerName = '';
 
 // set first question
 questionField.innerHTML = questionArray[0];
@@ -26,10 +27,10 @@ const changeQuestion = () => {
     answerField.value = '';
 
     // change question
-    if(questionIndex < questionArray.length) {
+    if (questionIndex < questionArray.length) {
         questionField.innerHTML = questionArray[questionIndex];
     } else {
-        questionField.innerHTML = `Total Score: ${total}`;
+        questionField.innerHTML = `You got ${total} out of 7 questions right! Thanks for playing ${playerName}`;
         nextBtn.style.display = 'none';
     }
 
@@ -43,7 +44,7 @@ const changeQuestion = () => {
  */
 const userAnswer = (answer, correctAnswer) => {
     // forcing a yes or no depending on what the user inputed
-    const answ = questionIndex > 4 ? answer :
+    const answ = questionIndex > 5 ? answer :
         answer.indexOf('y') !== -1 ? 'yes' :
             answer.indexOf('n') !== -1 ? 'no' :
                 '';
@@ -51,15 +52,15 @@ const userAnswer = (answer, correctAnswer) => {
     nextBtn.disabled = false;
     correctAnswer === answ ?
         (
-            resultField.innerHTML = `Your answer was: ${answer}, and that was correct!`,
+            resultField.innerHTML = `Your answer was: ${answer}, and that was <b>correct</b>!`,
             total++,
             console.log(`Question was: ${questionArray[questionIndex]}. User answered with: ${answ}`)
         ) : total !== 0 ? (
-            resultField.innerHTML = `Your answer was: ${answer}, and that was wrong :(`,
+            resultField.innerHTML = `Your answer was: ${answer}, and that was <b>wrong</b> :(`,
             total--,
             console.log(`User answered with: ${answ}`)
         ) :
-            resultField.innerHTML = `Your answer was: ${answer}, and that was wrong :(` ; // user has no points
+            resultField.innerHTML = `Your answer was: ${answer}, and that was <b>wrong</b> :(` ; // user has no points
 };
 
 /**
@@ -68,16 +69,24 @@ const userAnswer = (answer, correctAnswer) => {
  */
 const handleGame = (event) => {
     event.preventDefault();
-    const answer = event.target.answerField.value.toLowerCase();
+    const answer = event.target.answerField.value;
     const correctA = answerArray[questionIndex];
-    if(questionIndex < 5) {
-        if(possibleAnswersArray.indexOf(answer) !== -1) {
-            userAnswer(answer, correctA);
+
+    // change text on next button
+    nextBtn.value = 'Next Question';
+    if (questionIndex < 6) {
+        if (questionIndex === 0) {
+            playerName = answer;
+            resultField.innerHTML = `Hi ${answer}, let's start the game!`;
+            // enable next question button
+            nextBtn.disabled = false;
+        } else if (possibleAnswersArray.indexOf(answer) !== -1) {
+            userAnswer(answer.toLowerCase(), correctA);
         } else {
             resultField.innerHTML = 'Bad input, try again';
         }
     } else {
-        questionIndex === 5 ?
+        questionIndex === 6 ?
             (
                 answer === correctA ?
                     (
@@ -85,8 +94,8 @@ const handleGame = (event) => {
                     ) :
                     (
                         answer > correctA ?
-                            resultField.innerHTML = 'I would guess a little lower' :
-                            resultField.innerHTML = 'I would guess a little higher'
+                            resultField.innerHTML = 'I would guess a little <b>lower</b>' :
+                            resultField.innerHTML = 'I would guess a little <b>higher</b>'
                     )
             ) :
             (
