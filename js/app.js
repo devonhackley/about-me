22c63713d4e0d5c6ed70d726e012eed8c2e888e8
Devon Hackley <devonhackley@gmail.com>
2019-03-27 15:18:50 -0700

    updated styling and layout of about section, enabled next question button toggling

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // variables
-const questionArray = ['Do I like to code?', 'Was I in the military?', 'Do I have any siblings?', 'Do I like the rain?', 'Do I have a pet corgi'];
-const possibleAnswers = ['Yes', 'y', 'yeah', 'yes', 'No', 'n', 'nah', 'no'];
-const answerArray = ['yes', 'yes', 'yes', 'no', 'yes'];
+const questionArray = ['Do I like to code?', 'Was I in the military?', 'Do I have any siblings?', 'Do I like the rain?', 'Do I have a pet corgi', 'What is my favorite number?'];
+const possibleAnswersArray = ['Yes', 'y', 'yeah', 'yes', 'No', 'n', 'nah', 'no'];
+const answerArray = ['yes', 'yes', 'yes', 'no', 'yes', 8];
 const form = document.getElementById('guessingGame');
 const questionField = document.getElementById('gameQuestion');
 const resultField = document.getElementById('userResult');
@@ -32,6 +32,10 @@ const changeQuestion = () => {
         questionField.innerHTML = `Total Score: ${total}`;
         nextBtn.style.display = 'none';
     }
+
+    // disable next button
+    // enable next question button
+    nextBtn.disabled = true;
 };
 /**
  *  This function basically determines what to render for the result,
@@ -43,6 +47,8 @@ const userAnswer = (answer, correctAnswer) => {
     const answ = answer.indexOf('y') !== -1 ? 'yes' :
         answer.indexOf('n') !== -1 ? 'no' :
             '';
+    // enable next question button
+    nextBtn.disabled = false;
     correctAnswer === answ ?
         (
             resultField.innerHTML = `Your answer was: ${answer}, and that was correct!`,
@@ -63,10 +69,14 @@ const userAnswer = (answer, correctAnswer) => {
 const handleGame = (event) => {
     event.preventDefault();
     const answer = event.target.answerField.value.toLowerCase();
-    if(possibleAnswers.indexOf(answer) !== -1) {
-        userAnswer(answer, answerArray[questionIndex]);
+    if(questionIndex < 5) {
+        if(possibleAnswersArray.indexOf(answer) !== -1) {
+            userAnswer(answer, answerArray[questionIndex]);
+        } else {
+            resultField.innerHTML = 'Bad input, try again';
+        }
     } else {
-        resultField.innerHTML = 'Bad input, try again';
+        console.log('in here');
     }
 };
 
