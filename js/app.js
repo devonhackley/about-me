8aed8fb87e0e98e03609da263dddc699a9a9bb28
Devon Hackley <devonhackley@gmail.com>
2019-03-27 17:25:00 -0700

    Completed question 7

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // variables
-const questionArray = ['First, what is your name?','Do I like to code?', 'Was I in the military?', 'Do I have a sibling', 'Do I like the rain?', 'Do I have a pet corgi', 'What is my favorite number? Hint it is between 1-10'];
-const possibleAnswersArray = ['Yes', 'y', 'yeah', 'yes', 'No', 'n', 'nah', 'no'];
-const answerArray = ['yes', 'yes', 'yes', 'yes', 'no', 'yes', '8'];
+const questionArray = ['First, what is your name?','Do I like to code?', 'Was I in the military?', 'Do I have a sibling?', 'Do I like the rain?', 'Do I have a pet corgi?', 'What is my favorite number? Hint it is between 1-10', 'Can you guess what my favorite pizza toppings are?'];
+const possibleAnswersArray = ['Yes', 'y', 'Y', 'yeah', 'yes', 'No', 'n', 'N', 'nah', 'no'];
+const answerArray = ['yes', 'yes', 'yes', 'yes', 'no', 'yes', '8', ['pepperoni', 'cheese', 'onions']];
 const form = document.getElementById('guessingGame');
 const questionField = document.getElementById('gameQuestion');
 const resultField = document.getElementById('userResult');
@@ -12,6 +12,8 @@ const nextBtn = document.getElementById('nextQuestion');
 let questionIndex = 0;
 let total = 0;
 let playerName = '';
+let questionSixTries = 0;
+let questionSevenTries = 0;
 
 // set first question
 questionField.innerHTML = questionArray[0];
@@ -30,7 +32,12 @@ const changeQuestion = () => {
     if (questionIndex < questionArray.length) {
         questionField.innerHTML = questionArray[questionIndex];
     } else {
-        questionField.innerHTML = `You got ${total} out of 7 questions right! Thanks for playing ${playerName}`;
+        console.log(`The user's total was: ${total}`);
+        total === 7 ? // perfect
+            questionField.innerHTML = `You got ${total} out of 7 questions right! ${playerName}, that was a perfect score!` :
+            total === 0 ? // doesn't know me at all
+                questionField.innerHTML = `You got ${total} out of 7 questions right! ${playerName}, you didn't get anything right..` :
+                questionField.innerHTML = `You got ${total} out of 7 questions right! Thanks for playing ${playerName}!`; // got some questions right
         nextBtn.style.display = 'none';
     }
 
@@ -55,12 +62,11 @@ const userAnswer = (answer, correctAnswer) => {
             resultField.innerHTML = `Your answer was: ${answer}, and that was <b>correct</b>!`,
             total++,
             console.log(`Question was: ${questionArray[questionIndex]}. User answered with: ${answ}`)
-        ) : total !== 0 ? (
-            resultField.innerHTML = `Your answer was: ${answer}, and that was <b>wrong</b> :(`,
-            total--,
-            console.log(`User answered with: ${answ}`)
         ) :
-            resultField.innerHTML = `Your answer was: ${answer}, and that was <b>wrong</b> :(` ; // user has no points
+        (
+            console.log(`User answered with: ${answ}`),
+            resultField.innerHTML = `Your answer was: ${answer}, and that was <b>wrong</b> :(` // user has no points
+        );
 };
 
 /**
@@ -75,7 +81,7 @@ const handleGame = (event) => {
     // change text on next button
     nextBtn.value = 'Next Question';
     if (questionIndex < 6) {
-        if (questionIndex === 0) {
+        if (questionIndex === 0) { // first question is asking for the name of player
             playerName = answer;
             resultField.innerHTML = `Hi ${answer}, let's start the game!`;
             // enable next question button
@@ -93,13 +99,49 @@ const handleGame = (event) => {
                         userAnswer(answer, correctA)
                     ) :
                     (
-                        answer > correctA ?
-                            resultField.innerHTML = 'I would guess a little <b>lower</b>' :
-                            resultField.innerHTML = 'I would guess a little <b>higher</b>'
+                        questionSixTries < 4 ? // only allow the user to guess four times
+                            (
+                                answer > correctA ?
+                                    (
+                                        resultField.innerHTML = 'I would guess a little <b>lower</b>',
+                                        questionSixTries++,
+                                        console.log(`User has tried to answer the question ${questionSixTries} times.`)
+                                    ) :
+                                    (
+                                        resultField.innerHTML = 'I would guess a little <b>higher</b>',
+                                        questionSixTries++,
+                                        console.log(`User has tried to answer the question ${questionSixTries} times.`)
+                                    )
+                            ) :
+                            (
+                                resultField.innerHTML = 'Sorry that is all the tries you get',
+                                // enable next question button
+                                nextBtn.disabled = false
+                            )
                     )
             ) :
             (
-                console.log('blarg')
+
+                correctA.indexOf(answer.toLowerCase()) !== -1 ?
+                    ( // Paula reminded me of a great method arrays have, the .join(), annotating that here
+                        resultField.innerHTML = `Your answer was: ${answer}, and that was <b>correct</b>! Some other choices are: ${correctA.join(', ')}.`,
+                        total++,
+                        // enable next question button
+                        nextBtn.disabled = false
+                    ) :
+                    (
+                        questionSevenTries < 6 ? // only allow the user to guess four times
+                            (
+                                resultField.innerHTML = 'I would try to guess again!',
+                                questionSevenTries++,
+                                console.log(`User has tried to answer the question ${questionSevenTries} times.`)
+                            ) :
+                            (
+                                resultField.innerHTML = `Sorry that is all the tries you get. The correct answers are ${correctA.join(', ')}.`,
+                                // enable next question button
+                                nextBtn.disabled = false
+                            )
+                    )
             );
     }
 };
